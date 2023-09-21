2d08d701380810309234690e0843edb64e2b0d75
VVSD-Charan <20bcs244@iiitdmj.ac.in>
2023-09-21 22:44:52 +0530

    Added routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home.js';
+import Projects from './pages/Projects.js';
+import Courses from './pages/Courses.js';
+import Contact from './pages/Contact.js';
 
 function App() {
   return (
     <div className="App">
-        <h1>Portfolio</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/courses" element={<Courses />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
