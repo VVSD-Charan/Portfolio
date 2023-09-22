6c78d36cc3904f55235c68647b6fe6ed1cd46dbd
VVSD-Charan <20bcs244@iiitdmj.ac.in>
2023-09-22 10:48:17 +0530

    Added home page text

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,8 +3,16 @@ import React from 'react';
 function Home() {
   return (
     <div>
-      <div className='introduction' style={{ backgroundImage: `url('./background.svg')` }}>
-        {/* Your content here */}
+      <div className='introduction flex-with-center' style={{ backgroundImage: `url('./background.svg')` }}>
+         <div>
+            <h1>VVSD Charan</h1>
+
+            <div className='intro-content d-flex justify-content-between'>
+                <p>Software Developer C++ <br/> MERN Stack Developer</p>
+                <button className='primary-button'>Get Started</button>
+            </div>
+
+         </div>
       </div>
     </div>
   );
