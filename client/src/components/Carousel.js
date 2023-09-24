6955391004543f19aadaa4fff125edf48427e71f
VVSD-Charan <20bcs244@iiitdmj.ac.in>
2023-09-24 21:45:37 +0530

    Added transforms to skill sets

--- client/src/components/Carousel.js
+++ client/src/components/Carousel.js
@@ -11,14 +11,14 @@ function Carousel() {
                 <h1 className="position-absolute top-0 start-0 end-0 text-center mt-5">Technologies I use</h1>
 
                 <div className="gallery">
-                    <span><FaReact color="cyan"/></span>
-                    <span><FaBootstrap color="purple"/></span>
-                    <span><FaNodeJs color="green"/></span>
-                    <span><BsGit color="orange"/></span>
-                    <span><BsFiletypeSql color="blue"/></span>
-                    <span><FaJs color="yellow"/></span>
-                    <span><TbBrandCpp color="blue"/></span>
-                    <span><FaPhp color="purple"/></span>
+                    <span style={{'--i' : 1}}><FaReact color="cyan"/></span>
+                    <span style={{'--i' : 2}}><FaBootstrap color="#023e8a"/></span>
+                    <span style={{'--i' : 3}}><FaNodeJs color="green"/></span>
+                    <span style={{'--i' : 4}}><BsGit color="#e36414"/></span>
+                    <span style={{'--i' : 5}}><BsFiletypeSql color="#3c6e71"/></span>
+                    <span style={{'--i' : 6}}><FaJs color="yellow"/></span>
+                    <span style={{'--i' : 7}}><TbBrandCpp color="white"/></span>
+                    <span style={{'--i' : 8}}><FaPhp color="#003049"/></span>
                 </div>
 
             </div>
