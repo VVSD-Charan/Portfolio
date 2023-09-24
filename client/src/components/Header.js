2995f3c6cd1418f4081950fcc904a2b9dea8fae5
VVSD-Charan <20bcs244@iiitdmj.ac.in>
2023-09-24 16:05:54 +0530

    Added functionality to header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { RiMenu3Fill, RiCloseFill } from 'react-icons/ri'
+import { Link } from 'react-router-dom'
 
 function Header() {
     const [showHeader, setShowHeader] = useState(false);
@@ -13,6 +14,13 @@ function Header() {
                 <RiMenu3Fill onClick={()=>{setShowHeader(!showHeader)}} className="menu-icon position-fixed top-0 end-0" />
                 )
             }
+
+            <ul className={`${showHeader ? 'show-header' : 'hide-header'} n-box1`}>
+                <li><Link className='header-links'>Home</Link></li>
+                <li><Link className='header-links'>Projects</Link></li>
+                <li><Link className='header-links'>Courses</Link></li>
+                <li><Link className='header-links'>Contact</Link></li>
+            </ul>
 
         </div>
     )
