61d7b210870d1b0c948ace89cc5550cc9f10ec05
VVSD-Charan <20bcs244@iiitdmj.ac.in>
2023-09-24 23:06:08 +0530

    Added wave from getwave.io

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,21 +5,33 @@ import Carousel from '../components/Carousel';
 function Home() {
   return (
     <div>
-      <Header/>
 
+      {/* Header part (toggle button)  */}
+      <Header />
+
+      {/* Name and tagline  */}
       <div className='introduction flex-with-center' style={{ backgroundImage: `url('./background.svg')` }}>
-         <div>
-            <h1>VVSD Charan</h1>
+        <div>
+          <h1>VVSD Charan</h1>
 
-            <div className='intro-content d-flex justify-content-between'>
-                <p>Software Developer C++ <br/> MERN Stack Developer</p>
-                <button className='primary-button font-bold'>Get Started</button>
-            </div>
+          <div className='intro-content d-flex justify-content-between'>
+            <p>Software Developer C++ <br /> MERN Stack Developer</p>
+            <button className='primary-button font-bold'>Get Started</button>
+          </div>
 
-         </div>
+        </div>
       </div>
 
-      <Carousel/>
+      {/* Skillsets carousel  */}
+      <Carousel />
+
+      {/* Waves  */}
+      <div>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#001220" fill-opacity="1" d="M0,256L21.8,229.3C43.6,203,87,149,131,133.3C174.5,117,218,139,262,170.7C305.5,203,349,245,393,240C436.4,235,480,181,524,186.7C567.3,192,611,256,655,245.3C698.2,235,742,149,785,144C829.1,139,873,213,916,256C960,299,1004,309,1047,304C1090.9,299,1135,277,1178,229.3C1221.8,181,1265,107,1309,106.7C1352.7,107,1396,181,1418,218.7L1440,256L1440,0L1418.2,0C1396.4,0,1353,0,1309,0C1265.5,0,1222,0,1178,0C1134.5,0,1091,0,1047,0C1003.6,0,960,0,916,0C872.7,0,829,0,785,0C741.8,0,698,0,655,0C610.9,0,567,0,524,0C480,0,436,0,393,0C349.1,0,305,0,262,0C218.2,0,175,0,131,0C87.3,0,44,0,22,0L0,0Z"></path></svg>
+      </div>
+
+      {/* about  */}
+      
     </div>
   );
 }
