f9901cf6bc996b99362739ed77522d01badb21ed
VVSD-Charan <20bcs244@iiitdmj.ac.in>
2023-10-01 13:37:27 +0530

    Added skills section

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -15,7 +15,7 @@ function Home() {
           <h1>VVSD Charan</h1>
 
           <div className='intro-content d-flex justify-content-between'>
-            <p>Software Developer C++ <br /> MERN Stack Developer</p>
+            <p>Software Developer C++ and <br /> a MERN Stack Developer</p>
             <button className='primary-button font-bold'>Get Started</button>
           </div>
 
@@ -36,7 +36,7 @@ function Home() {
         <div className='row justify-content-start'>
           <div className='col-md-4'>
             <div className='n-box2 px-4 py-4 font-bold'>
-              <h1>Yes you are right !</h1> <br/>
+              <h1>Yes you are right !</h1> <br />
               <p className='desc'>I am a Software developer</p>
             </div>
           </div>
@@ -52,7 +52,7 @@ function Home() {
           <div className='col-md-4'>
             <div className='n-box2 px-5 py-4'>
               <p className='fs-17'>
-                Crafting Code and Shaping Solutions: I'm a Software Developer, Turning Ideas into Digital Realities, One Line of Code at a Time.
+                Crafting Code and Shaping Solutions! <br/> I'm a Software Developer, Turning Ideas into Digital Realities, One Line of Code at a Time.
               </p>
             </div>
           </div>
@@ -60,6 +60,56 @@ function Home() {
 
       </div>
 
+      {/* Skills section  */}
+      <div className='devstack container mt-5 n-box2 py-5'>
+
+          <div className='text-center'>
+            <h2 className='font-bold mb-5'>My Skill-sets</h2>
+            <img src='./developer.svg' height='200px' width='200px'/>
+          </div>
+
+          <div className='row mt-5'>
+            <div className='col-md-4'>
+                <div className='font-bold text-center'>
+                  <h4 className='font-bold'>Front-end</h4>
+                  <hr/>
+                  <p>HTML</p>
+                  <p>JavaScript</p>
+                  <p>ReactDOM</p>
+                  <p>Embedded JavaScript</p>
+                  <p>Redux</p>
+                  <p>CSS</p>
+                  <p>Bootstrap</p>
+                </div>
+            </div>
+
+            <div className='col-md-4'>
+                <div className='font-bold text-center'>
+                  <h4 className='font-bold'>Back-end / Databases</h4>
+                  <hr/>
+                  <p>Node.JS</p>
+                  <p>Express.JS</p>
+                  <p>PHP</p>
+                  <p>SQL</p>
+                  <p>MongoDB</p>
+                </div>
+            </div>
+
+            <div className='col-md-4'>
+                <div className='font-bold text-center'>
+                  <h4 className='font-bold'>Other</h4>
+                  <hr/>
+                  <p>C/C++</p>
+                  <p>Python</p>
+                  <p>Data Structures and Algorithms</p>
+                  <p>Object Oriented Programming Concepts</p>
+                  <p>DataBase Management System</p>
+                  <p>Operating System</p>
+                  <p>Computer networks</p>
+                </div>
+            </div>
+          </div>
+      </div>
     </div>
   );
 }
