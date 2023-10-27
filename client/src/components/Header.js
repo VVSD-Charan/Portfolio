3c32f30d70b2e2c41904645f1f42fb81bbbb87d1
VVSD-Charan <20bcs244@iiitdmj.ac.in>
2023-10-27 15:55:53 +0530

    modified:   client/src/components/Header.js

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -19,8 +19,8 @@ function Header() {
             <ul className={`${showHeader ? 'show-header' : 'hide-header'} n-box1`}>
                 <li className={`${path === '/' && 'active'}`}><Link to='/' className='header-links'>Home</Link></li>
                 <li className={`${path === '/projects' && 'active'}`}><Link to='/projects' className='header-links'>Projects</Link></li>
-                <li className={`${path === '/courses' && 'active'}`}><Link to='/courses' className='header-links'>Courses</Link></li>
                 <li className={`${path === '/contact' && 'active'}`}><Link to='/contact' className='header-links'>Contact</Link></li>
+                <li className={`${path === '/courses' && 'active'}`}><Link to='/courses' className='header-links'>Info and Course-work</Link></li>
             </ul>
 
         </div>
