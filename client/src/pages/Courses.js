49d34177ef112c7333aa2c85d251ea061c7f1ce0
VVSD-Charan <20bcs244@iiitdmj.ac.in>
2023-10-27 15:53:30 +0530

    Courses intro

--- client/src/pages/Courses.js
+++ client/src/pages/Courses.js
@@ -3,7 +3,19 @@ import React from 'react'
 function Courses() {
     return (
         <div>
-            <h1>Courses</h1>
+            <div className='courses-intro' style={{ backgroundImage: `url('./courses.svg')` }}>
+                <div className='courses-intro-content'>
+                    <h1>About Me</h1>
+
+                    <div>
+                        <p>
+                            "I'm an adept programmer, skilled in communication and adaptable. Proficient in Frontend and Backend technologies, with MERN Stack expertise. Strong in Data Structures, Algorithms, and collaborative problem-solving. Committed to team success, effective in building social connections."
+                        </p>
+
+                        <button className='primary-button'>Get Started</button>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
