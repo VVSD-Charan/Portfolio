139f8e6d367c18e3d5641f880040472e72c1c317
VVSD-Charan <20bcs244@iiitdmj.ac.in>
2023-11-03 00:56:01 +0530

    Added courses data

---
+++ client/src/pages/coursesdata.js
@@ -0,0 +1,72 @@
+const courses = [
+    {
+        title : "React.JS",
+        image : "./reactjs.svg",
+    },
+    {
+        title : "Node.JS",
+        image : "./nodejs.svg",
+    },
+    {
+        title : "Express.JS",
+        image : "./expressjs.svg",
+    },
+    {
+        title : "MongoDB",
+        image : './mongodb.svg',
+    },
+    {
+        title : "MySQL",
+        image : './mysql.svg',
+    },
+    {
+        title : "JavaScript",
+        image : "./javascript.svg",
+    },
+    {
+        title : "API integration",
+        image : "./api.svg"
+    },
+    {
+        title : "PHP",
+        image : "./php.svg",
+    },
+    {
+        title : "CSS",
+        image : "./css.svg",
+    },
+    {
+        title : "HTML",
+        image : "./html.svg",
+    },
+    {
+        title : "C++",
+        image : "./cpp.svg",
+    },
+    {
+        title : "Data structures and Algorithms",
+        image : "./dsa.svg",
+    },
+    {
+        title : "Competitive programming",
+        image : "./cp.svg",
+    },
+    {
+        title : "Python",
+        image : "./python.svg",
+    },
+    {
+        title : "Database Management",
+        image : "./dbms.svg"
+    },
+    {
+        title : "Operating System",
+        image : "./os.svg",
+    },
+    {
+        title : "Computer Networks",
+        image : "./cn.svg",
+    },
+];
+
+export default courses;
