8f84b90687dd2494fb025f162ab7d69d710ac35a
VVSD-Charan <20bcs244@iiitdmj.ac.in>
2023-11-03 00:03:34 +0530

    Added header to contact page

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Header from '../components/Header';
 
 function Contact() {
     return (
         <div>
+            <Header/>
             <div className='container contact mt-5'>
                 <div className='row pt-5'>
                     <div className='col-md-6 p-5'>
