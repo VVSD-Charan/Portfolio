b2ad747635cd3e6a9235ae08cf9649d33e9f09c0
VVSD-Charan <20bcs244@iiitdmj.ac.in>
2023-11-03 12:34:17 +0530

    Fixed contact form bug

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -4,23 +4,24 @@ import Header from '../components/Header';
 function Contact() {
     return (
         <div>
-            <Header/>
+            <Header />
             <div className='container contact mt-5'>
                 <div className='row pt-5'>
                     <div className='col-md-6 p-5'>
-                        <img src='./contactme.svg' alt='contact-us'/>
+                        <img src='./contactme.svg' alt='contact-us' />
                     </div>
 
                     <div className='col-md-6'>
                         <form className='contact-form m-2 p-5 n-box2' action='mailto:vvsdcharan2001@gmail.com' method='post' encType='text/plain'>
                             <h3 className='font-bold'>Contact Me</h3>
-                            <hr/>
-                            <input type='text' className='form-control' placeholder='Name'/>
-                            <input type='email' className='form-control' placeholder='Email'/>
-                            <textarea className='form-control' rows={3} defaultValue={""} placeholder='Description'/>
+                            <hr />
+                            <input type='text' className='form-control' name='Name' placeholder='Name' />
+                            <input type='email' className='form-control' name='Email' placeholder='Email' />
+                            <textarea className='form-control' name='Description' rows={3} placeholder='Description'></textarea>
 
-                            <button className='primary-button mt-3'>SUBMIT</button>
+                            <button className='primary-button mt-3' type='submit'>SUBMIT</button>
                         </form>
+
                     </div>
                 </div>
             </div>
