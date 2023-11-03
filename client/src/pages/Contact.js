3162dcbdbe7b76ec7c27f607179e47b3b2a16d6c
VVSD-Charan <20bcs244@iiitdmj.ac.in>
2023-11-03 12:43:32 +0530

    Fixed contact page bug

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,32 +1,44 @@
-import React from 'react'
+import React from 'react';
 import Header from '../components/Header';
 
 function Contact() {
-    return (
-        <div>
-            <Header />
-            <div className='container contact mt-5'>
-                <div className='row pt-5'>
-                    <div className='col-md-6 p-5'>
-                        <img src='./contactme.svg' alt='contact-us' />
-                    </div>
+  // Define a function to reload the page
+  const reloadPage = () => {
+    window.location.reload();
+  };
 
-                    <div className='col-md-6'>
-                        <form className='contact-form m-2 p-5 n-box2' action='mailto:vvsdcharan2001@gmail.com' method='post' encType='text/plain'>
-                            <h3 className='font-bold'>Contact Me</h3>
-                            <hr />
-                            <input type='text' className='form-control' name='Name' placeholder='Name' />
-                            <input type='email' className='form-control' name='Email' placeholder='Email' />
-                            <textarea className='form-control' name='Description' rows={3} placeholder='Description'></textarea>
+  return (
+    <div>
+      <Header />
+      <div className='container contact mt-5'>
+        <div className='row pt-5'>
+          <div className='col-md-6 p-5'>
+            <img src='./contactme.svg' alt='contact-us' />
+          </div>
 
-                            <button className='primary-button mt-3' type='submit'>SUBMIT</button>
-                        </form>
+          <div className='col-md-6'>
+            <form
+              className='contact-form m-2 p-5 n-box2'
+              action='mailto:vvsdcharan2001@gmail.com'
+              method='post'
+              encType='text/plain'
+              onSubmit={reloadPage} // Assign the function to onSubmit
+            >
+              <h3 className='font-bold'>Contact Me</h3>
+              <hr />
+              <input type='text' className='form-control' name='Name' placeholder='Name' />
+              <input type='email' className='form-control' name='Email' placeholder='Email' />
+              <textarea className='form-control' name='Description' rows={3} placeholder='Description'></textarea>
 
-                    </div>
-                </div>
-            </div>
+              <button className='primary-button mt-3' type='submit'>
+                SUBMIT
+              </button>
+            </form>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
 export default Contact;
