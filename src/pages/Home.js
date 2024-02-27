68007e17f25991ac8f0592f86321d96f42bbf9e5
VVSD-Charan <20bcs244@iiitdmj.ac.in>
2024-02-27 08:53:29 +0530

    Fixed css

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,13 +11,32 @@ function Home() {
       <Header />
 
       {/* Name and tagline  */}
-      <div className='introduction flex-with-center' style={{ backgroundImage: `url('./background.svg')` }}>
+      <div className='introduction flex-with-center' style={{ backgroundColor: 'black' }}>
         <div>
-          <h1 data-aos='fade-up'>VVSD Charan</h1>
+          <svg className='animated-svg' viewBox="0 0 960 300">
+            <symbol id="s-text">
+              <text textAnchor="middle" x="50%" y="80%">VVSD Charan</text>
+            </symbol>
+            <g className="g-ants">
+              <use xlinkHref="#s-text" className="text-copy" />
+              <use xlinkHref="#s-text" className="text-copy" />
+              <use xlinkHref="#s-text" className="text-copy" />
+              <use xlinkHref="#s-text" className="text-copy" />
+              <use xlinkHref="#s-text" className="text-copy" />
+            </g>
+          </svg>
 
-          <div className='intro-content d-flex justify-content-between'>
-            <p>Software Developer C++ and <br /> a MERN Stack Developer</p>
-            <button className='primary-button font-bold'><Link to='https://drive.google.com/file/d/1m3QAKV2U_6jeGP3wQJ1d_y5Sx368iTea/view?usp=drive_link' target='_blank' style={{textDecoration:"none",color:"black",fontWeight:'bold'}}>My Resume</Link></button>
+          <div className='intro-content d-flex' style={{ justifyContent: 'space-evenly' }}>
+            <div className='animated-description'>
+              I am a
+              <div className="dropping-texts">
+                <div> MERN stack developer</div>
+                <div> C++ developer</div>
+                <div> Competitive programmer</div>
+                <div> PHP-SQL developer</div>
+              </div>
+            </div>
+            <button className='primary-button font-bold'><Link to='https://drive.google.com/file/d/1m3QAKV2U_6jeGP3wQJ1d_y5Sx368iTea/view?usp=drive_link' target='_blank' style={{ textDecoration: "none", color: 'white', fontWeight: 'bold' }}>My Resume</Link></button>
           </div>
 
         </div>
@@ -28,37 +47,7 @@ function Home() {
 
       {/* Waves  */}
       <div>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#001220" fill-opacity="1" d="M0,256L21.8,229.3C43.6,203,87,149,131,133.3C174.5,117,218,139,262,170.7C305.5,203,349,245,393,240C436.4,235,480,181,524,186.7C567.3,192,611,256,655,245.3C698.2,235,742,149,785,144C829.1,139,873,213,916,256C960,299,1004,309,1047,304C1090.9,299,1135,277,1178,229.3C1221.8,181,1265,107,1309,106.7C1352.7,107,1396,181,1418,218.7L1440,256L1440,0L1418.2,0C1396.4,0,1353,0,1309,0C1265.5,0,1222,0,1178,0C1134.5,0,1091,0,1047,0C1003.6,0,960,0,916,0C872.7,0,829,0,785,0C741.8,0,698,0,655,0C610.9,0,567,0,524,0C480,0,436,0,393,0C349.1,0,305,0,262,0C218.2,0,175,0,131,0C87.3,0,44,0,22,0L0,0Z"></path></svg>
-      </div>
-
-      {/* about  */}
-      <div className='container'>
-
-        <div className='row justify-content-start'>
-          <div className='col-md-4'>
-            <div className='n-box2 px-4 py-4 font-bold' data-aos='fade-right'>
-              <h1>Yes you are right !</h1> <br />
-              <p className='desc'>I am a Software developer</p>
-            </div>
-          </div>
-        </div>
-
-        <div className='row pt-5 justify-content-center'>
-          <div className='col-md-4'>
-            <img src='./jsbuff.svg' alt='' height="200" className='w-100'></img>
-          </div>
-        </div>
-
-        <div className='row pt-5 justify-content-end'>
-          <div className='col-md-4'>
-            <div className='n-box2 px-5 py-4' data-aos='fade-left'>
-              <p className='fs-17'>
-                Crafting Code and Shaping Solutions! <br /> I'm a Software Developer, Turning Ideas into Digital Realities, One Line of Code at a Time.
-              </p>
-            </div>
-          </div>
-        </div>
-
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="black" fill-opacity="1" d="M0,256L21.8,229.3C43.6,203,87,149,131,133.3C174.5,117,218,139,262,170.7C305.5,203,349,245,393,240C436.4,235,480,181,524,186.7C567.3,192,611,256,655,245.3C698.2,235,742,149,785,144C829.1,139,873,213,916,256C960,299,1004,309,1047,304C1090.9,299,1135,277,1178,229.3C1221.8,181,1265,107,1309,106.7C1352.7,107,1396,181,1418,218.7L1440,256L1440,0L1418.2,0C1396.4,0,1353,0,1309,0C1265.5,0,1222,0,1178,0C1134.5,0,1091,0,1047,0C1003.6,0,960,0,916,0C872.7,0,829,0,785,0C741.8,0,698,0,655,0C610.9,0,567,0,524,0C480,0,436,0,393,0C349.1,0,305,0,262,0C218.2,0,175,0,131,0C87.3,0,44,0,22,0L0,0Z"></path></svg>
       </div>
 
       {/* Skills section  */}
@@ -66,7 +55,7 @@ function Home() {
 
         <div className='text-center'>
           <h2 className='font-bold mb-5'>My Skill-sets</h2>
-          <img src='./developer.svg' height='200px' width='200px' alt='developer'/>
+          <img src='./developer.svg' height='200px' width='200px' alt='developer' />
         </div>
 
         <div className='row mt-5'>
@@ -74,9 +63,10 @@ function Home() {
             <div className='font-bold text-center'>
               <h4 className='font-bold'>Front-end</h4>
               <hr />
-              <p>HTML</p>
+              <p></p>
               <p>JavaScript</p>
-              <p>ReactDOM</p>
+              <p>Next.JS</p>
+              <p>React.JS</p>
               <p>Embedded JavaScript</p>
               <p>Redux</p>
               <p>CSS</p>
@@ -101,7 +91,6 @@ function Home() {
               <h4 className='font-bold'>Other</h4>
               <hr />
               <p>C/C++</p>
-              <p>Python</p>
               <p>Data Structures and Algorithms</p>
               <p>Object Oriented Programming Concepts</p>
               <p>DataBase Management System</p>
@@ -112,23 +101,6 @@ function Home() {
         </div>
       </div>
 
-      <div>
-        <h3 className='font-bold text-center'>Who is Charan?</h3>
-        <div className='who-am-i flex-with-center' style={{ backgroundImage: `url('/blob.svg')` }}>
-          <div>
-            <h3 className='text-center'>Hello!</h3><br />
-            <p>
-              <pre>{JSON.stringify({
-                Name: "VVSD Charan",
-                From: "Hyderabad, India",
-                Age: "21",
-                Gender: "male",
-                Insitute:"IIITDM Jabalpur"
-              }, null, 2)}</pre>
-            </p>
-          </div>
-        </div>
-      </div>
     </div>
   );
 }
